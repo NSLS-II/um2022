29e233408678b72257e7c53f1e9efe6172dd3232
abarbour <abarbour@bnl.gov>
2022-05-25 06:31:02 -0400

    first push no major changes need should troubleshoot 1 more thing if time and format needed

---
+++ steady_as_beamlines_go/anomaly/load_data.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+from .extract_features import get_features_single_datum
+from pathlib import Path
+import glob
+# import random
+
+
+def process_files(path):# save_file_name="CSX_data.csv"):
+    """
+    Reads raw data, generate features, construct a pandas DataFrame 
+
+    Parameters
+    ----------
+    path : Path
+        location of the raw hdf5 files.
+    save_file_name : string, optional
+        name of the file to save the data frame. The default is 'CSX_data.csv'.
+
+    Returns
+    -------
+    data_frame : pandas.DataFrame
+        processed data.
+
+    """
+
+    df = []
+    #files = list(path.glob("*.h5")) -- glob library change?
+    files = list(glob.glob(f"{path}*.h5"))
+    print(files)
+    for file in files:
+
+        d = pd.read_hdf(file, key="scan")
+        features = get_features_single_datum(d)
+        df.append(features)
+
+    data_frame = pd.DataFrame(df)
+    #data_frame.to_csv(save_file_name)
+    #print(f"data is processed and saved in {save_file_name}")
+    return data_frame
